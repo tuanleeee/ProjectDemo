834f3a5986e6caf1bee56930d22f41ea0c633c17
Quan <leminhquan8101998@gmail.com>
2019-07-16 17:36:18 +0700

    create login and mod sign up (everyone can sign up)

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,9 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+
+Route::post('/login','AuthController@login');
+Route::post('/signup','AuthController@signup');
+Route::group(['middleware'=>'auth:api'],function(){
+    Route::get('/userDetails','AuthController@user');
 });
